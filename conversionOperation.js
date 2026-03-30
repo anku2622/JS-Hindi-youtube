dbf3f748f250559321a5621a02090557e83e89db
anku2622 <anku42237@gmail.com>
2026-03-30 19:36:40 +0530

    Add files via upload

---
+++ conversionOperation.js
@@ -0,0 +1,26 @@
+// let score = 33
+// let score = "33abc"
+// let score = null
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" => easily convert into number 33
+//"33abc" => NaN
+//true => 1 ; false => 0
+
+// let Number = 1
+// let Number = ""
+// let Number = "Anku"
+// let convertBoolean = Boolean(Number)
+// console.log(convertBoolean)
+
+//1 => boolean me true me convert hota h ; 0=> false
+//""(empty string) => false ; "Anku" => true
+
+let someNumber = 22
+let StringNumber = String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
