42e2040dcb01ef4208d48b57d226104c737027d9
Anku Yadav <anku42237@gmail.com>
2026-04-12 15:18:53 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,32 @@
+//array
+const myArr = [0,1,2,6,4,5];
+console.log(myArr[0]);
+console.log(myArr[1]);
+console.log(myArr[3]);
+//to declare array
+const Heroes = ["shaktiman" , "nagraj"];
+const arr = new Array(1,2,3,4,5,6);
+// arr.push(10);
+// console.log(arr);
+
+// arr.pop(); 
+
+// arr.unshift(10);
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// console.log(arr.includes(5));
+// console.log(arr.indexOf(6));
+
+// console.log(arr);
+// console.log(arr.join())
+// console.log(typeof arr.join());
+
+const Arr = [1,2,3,4,5,6,19];
+console.log(Arr.slice(1,4));
+console.log(Arr);
+console.log(Arr.splice(1,4));
+console.log(Arr);
+
